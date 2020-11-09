e195ad70cd6320f42df327e747cae6538c5705ec
Luca Bodini <lucabodd@gmail.com>
2020-11-09 15:07:26 +0100

    ufa codebase

--- playground.js
+++ playground.js
@@ -1,43 +1,54 @@
-//Configurations
-const config = require('./etc/config.json');
-//LDAP
-var LDAP = require("./modules/ldap");
-var ldap = new LDAP(config.ldap);
-var ldap2 = new LDAP(config.ldap);
+const U2F = require("u2f");
+const Express = require("express");
+const BodyParser = require("body-parser");
+const Cors = require("cors");
+const HTTPS = require("https");
+const FS = require("fs");
+const session = require("express-session");
+
+const APP_ID = "https://maics.hqit.kaleyra.com";
+
+var app = Express();
+
+app.use(session({ secret: "thepolyglotdeveloper", cookie: { secure: true, maxAge: 60000 }, saveUninitialized: true, resave: true }));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(Cors({ origin: [APP_ID], credentials: true }));
+
+var user;
+
+app.get("/register", (request, response, next) => {
+    request.session.u2f = U2F.request(APP_ID);
+    response.send(request.session.u2f);
+});
+
+app.post("/register", (request, response, next) => {
+    var registration = U2F.checkRegistration(request.session.u2f, request.body.registerResponse);
+    if(!registration.successful) {
+        return response.status(500).send({ message: "error" });
+    }
+    user = registration;
+    response.send({ message: "The hardware key has been registered" });
+});
+
+app.get("/login", (request, response, next) => {
+    request.session.u2f = U2F.request(APP_ID, user.keyHandle);
+    response.send(request.session.u2f);
+});
+
+app.get("/", (request, response, next) => {
+    response.sendFile("index.html", {root: __dirname });
+});
 
 
-uid="luca.bodini"
-ldap.getUserGroups(uid).then(
-    function(res){console.log(res)}
-);
+app.post("/login", (request, response, next) => {
+    var success = U2F.checkSignature(request.session.u2f, request.body.loginResponse, user.publicKey);
+    response.send(success);
+});
 
-p2 = ldap.delGroup("Administrators")
-p1 = ldap2.delGroup("Developers")
-
-Promise.all([p1, p2])
-  .then(
-      function(ldap_rset){
-          console.log(ldap_rset)
-      },
-      function(err){
-          console.log(err)
-      }
- )
- .catch(function(err) {
-               // log that I have an error, return the entire array;
-               console.log('A promise failed to resolve', err);
-               return arrayOfPromises;
-           });
-
-/*
-console.log("SEARCHGRP")
- ldap.searchGrp({ scope: 'sub', filter: '(cn=*)', attributes: ['gidNumber']})
-   .then(
-       function(ldap_rset){
-           console.log(ldap_rset)
-       },
-       function(err){
-           console.log(err)
-       }
-  );
-*/
+HTTPS.createServer({
+    key: FS.readFileSync("ssl/https/key.pem"),
+    cert: FS.readFileSync("ssl/https/certificate.pem")
+}, app).listen(443, () => {
+    console.log("Listening at :443...");
+});
--- routes/login.js
+++ routes/login.js
@@ -42,6 +42,13 @@ schema
 const util = require('util');
 var nodemailer = require('nodemailer');
 const mail = require('../etc/mailtemplates.json');
+const transporter = nodemailer.createTransport({
+    port: 25,
+    host: 'localhost',
+    tls: {
+        rejectUnauthorized: false
+    }
+ });
 
 /**************************************
 *  Contain routes related to login   *
@@ -50,7 +57,7 @@ const mail = require('../etc/mailtemplates.json');
 
 
 /* GET login page
-* when unauth users go to SKDC lands on this page*/
+* when unauth users go to MAICS lands on this page*/
 router.get('/login', function(req, res, next) {
     req.session.email = undefined;
     req.session.role = undefined;
@@ -66,103 +73,129 @@ router.post("/password-reset", function (req,res,next) {
     mdb.connect(mongo_instance)
     .then(
         function(){
-            mdb.findDocument("users", {sys_username: uid}, {email:1})
+            mdb.findDocument("users", {sys_username: uid}, {email:1, pwdAccountLockedTime: 1})
             .then(
                 function(user){
                     if(user != undefined)
                     {
-                        ldap.modPwd(uid, pwd)
-                        .then(
-                            function () {
-                                mdb.connect(mongo_instance)
-                                .then(
-                                    function(){
-                                        var now = new Date();
-                                        mdb.updDocument("users", {"email": user.email}, {$set: {"pwdChangedTime": now.toISOString().replace(/-/g,"").replace("T","").replace(/:/g,"").slice(0,-5)+"Z"}});
-                                        log('[+] Password change password update time for user'+user.email, app_log);
-                                    },
-                                    function(err){
-                                        log('[-] Could not change password update time for user '+user.email+' this is not a fatal error but user account might be locked every 10 minutes', app_log);
-                                    }
-                                )
-                                //sending Mail
-                                const transporter = nodemailer.createTransport({
-                                    port: 25,
-                                    host: 'localhost',
-                                    tls: {
-                                        rejectUnauthorized: false
-                                    }
-                                 });
-                                 //send mail to admin
-                                 text="User "+uid+" asked for a password reset. If you think this is a suspicious activity, please log in and lock account as soon as possible.\nRequest occurred from: "+req.ip.replace(/f/g, "").replace(/:/g, "")+" User agent: "+req.get('User-Agent');
-                                 admin_mail = "SKDC <"+config.maics.admin_mail+">"
-                                 admins_mail = "Admins <"+config.maics.admin_mail+">"
-                                 var message = {
-                                    from: admin_mail,
-                                    to: admins_mail,
-                                    subject: "SKDC - User "+uid+" asked for password reset",
-                                    html: util.format(mail.noreset , text)
-                                 };
-                                 transporter.sendMail(message, (error, info) => {
-                                    if (error) {
-                                        log('[-] Could not send mail to '+config.maics.admin_mail+': '+error, app_log);
-                                    }
-                                    else {
-                                        log('[+] Admin warning mail sent to '+config.maics.admin_mail+'.', app_log);
-                                    }
-                                  });
-                                 //send mail to user
-                                 text="Password for user "+uid+" has been reset successfully. Your temporary password is: "+pwd+" please change it by clicking on the button below and follow the procedure.\n ";
-                                 admin_mail = "SKDC <"+config.maics.admin_mail+">"
-                                 user_mail = uid+" <"+user.email+">"
-                                 var message = {
-                                    from: admin_mail,
-                                    to: user_mail,
-                                    subject: "SKDC - User password "+uid+" reset",
-                                    html: util.format(mail.noreset , text)
-                                 };
+                        if(user.pwdAccountLockedTime == ""){
+                            ldap.modPwd(uid, pwd)
+                            .then(
+                                function () {
+                                    mdb.connect(mongo_instance)
+                                    .then(
+                                        function(){
+                                            mdb.updDocument("users", {"email": user.email}, {$set: {"pwdChangedTime": ztime.current()}});
+                                            log('[+] Password change password update time for user'+user.email, app_log);
+                                        },
+                                        function(err){
+                                            log('[-] Could not change password update time for user '+user.email+' this is not a fatal error but user account might be locked every 10 minutes', app_log);
+                                        }
+                                    )
 
-                                 transporter.sendMail(message, (error, info) => {
-                                    if (error) {
-                                        log('[-] LDAP password has been resetted but mail to '+user.email+' could not be sent, reason: '+err.message, app_log);
-                                        res.render('error-500', {
-                                            message: "500",
-                                            error: {status: "Service Unvailable", detail: "Server cannot send mail"}
-                                        });
-                                    }
-                                    else {
-                                        log('[+] LDAP password has been successfully resetted mail to '+user.email+' sent.', app_log);
-                                        log('[+] LDAP password has been successfully resetted and sent to '+user.email+' sent. request arrived from '+req.ip.replace(/f/g, "").replace(/:/g, "")+' User-Agent : '+req.get('User-Agent'), journal_log);
-                                        res.redirect("/login");
-                                    }
-                                  });
-                            },
-                            function (err) {
-                                log('[*] cannot modify LDAP password unser might not exist, reason: '+err.message, app_log);
-                                res.render('error-500', {
-                                    message: "500",
-                                    error: {status: "Service Unvailable", detail: "Server cannot handle your request"}
-                                });
-                            }
-                        );
-                    }
+                                     //send mail to admin
+                                     text="User "+uid+" asked for a password reset. If you think this is a suspicious activity, please log in and lock account as soon as possible.\nRequest occurred from: "+req.ip.replace(/f/g, "").replace(/:/g, "")+" User agent: "+req.get('User-Agent');
+                                     admin_mail = "MAICS <"+config.maics.admin_mail+">"
+                                     admins_mail = "Admins <"+config.maics.admin_mail+">"
+                                     var message = {
+                                        from: admin_mail,
+                                        to: admins_mail,
+                                        subject: "MAICS - User "+uid+" asked for password reset",
+                                        html: util.format(mail.noreset , text)
+                                     };
+                                     transporter.sendMail(message, (error, info) => {
+                                        if (error) {
+                                            log('[-] Could not send mail to '+config.maics.admin_mail+': '+error, app_log);
+                                        }
+                                        else {
+                                            log('[+] Admin warning mail sent to '+config.maics.admin_mail+'.', app_log);
+                                        }
+                                      });
+                                     //send mail to user
+                                     text="Password for user "+uid+" has been reset successfully. Your temporary password is: "+pwd+" please change it by clicking on the button below and follow the procedure.\n ";
+                                     admin_mail = "MAICS <"+config.maics.admin_mail+">"
+                                     user_mail = uid+" <"+user.email+">"
+                                     var message = {
+                                        from: admin_mail,
+                                        to: user_mail,
+                                        subject: "MAICS - User password "+uid+" reset",
+                                        html: util.format(mail.noreset , text)
+                                     };
+
+                                     transporter.sendMail(message, (error, info) => {
+                                        if (error) {
+                                            log('[-] LDAP password has been resetted but mail to '+user.email+' could not be sent, reason: '+err.message, app_log);
+                                            res.render('error-500', {
+                                                message: "500",
+                                                error: {status: "Service Unvailable", detail: "Server cannot send mail"}
+                                            });
+                                        }
+                                        else {
+                                            log('[+] LDAP password has been successfully resetted mail to '+user.email+' sent.', app_log);
+                                            log('[+] LDAP password has been successfully resetted and sent to '+user.email+' sent. request arrived from '+req.ip.replace(/f/g, "").replace(/:/g, "")+' User-Agent : '+req.get('User-Agent'), journal_log);
+                                            res.redirect("/login");
+                                        }
+                                      });
+                                },
+                                function (err) {
+                                    log('[*] cannot modify LDAP password unser might not exist, reason: '+err.message, app_log);
+                                    res.render('error-500', {
+                                        message: "500",
+                                        error: {status: "Service Unvailable", detail: "Server cannot handle your request"}
+                                    });
+                                }
+                            );
+                        }//end of if for pwdAccountLockedTime
+                        else {
+                            //send mail to admin
+                            text="User "+uid+" asked for a password reset. Unfortunately MAICS cannot change password for this user because is locked due to password expiration or policy.\nIf you think this request is legitimate please login to MAICS and unlock user account.\nRequest occurred from: "+req.ip.replace(/f/g, "").replace(/:/g, "")+" User agent: "+req.get('User-Agent');
+                            admin_mail = "MAICS <"+config.maics.admin_mail+">"
+                            admins_mail = "Admins <"+config.maics.admin_mail+">"
+                            var message = {
+                               from: admin_mail,
+                               to: admins_mail,
+                               subject: "MAICS - Locked user "+uid+" asked for password reset",
+                               html: util.format(mail.noreset , text)
+                            };
+                            transporter.sendMail(message, (error, info) => {
+                               if (error) {
+                                   log('[-] Could not send mail to '+config.maics.admin_mail+': '+error, app_log);
+                               }
+                               else {
+                                   log('[+] Admin warning mail sent to '+config.maics.admin_mail+'.', app_log);
+                               }
+                             });
+                             //send mail to user
+                             text="Password reset for "+uid+" has been requested. Unfortunately MAICS cannot change password for this user because it is locked.\nPlease ask administrators to unlock your account";
+                             admin_mail = "MAICS <"+config.maics.admin_mail+">"
+                             admins_mail = "Admins <"+config.maics.admin_mail+">"
+                             var message = {
+                                from: admin_mail,
+                                to: user.email,
+                                subject: "MAICS - Locked user "+uid+" asked for password reset",
+                                html: util.format(mail.noreset , text)
+                             };
+                             transporter.sendMail(message, (error, info) => {
+                                if (error) {
+                                    log('[-] Could not send mail to '+config.maics.admin_mail+': '+error, app_log);
+                                }
+                                else {
+                                    log('[+] Admin warning mail sent to '+config.maics.admin_mail+'.', app_log);
+                                }
+                              });
+                              res.redirect("/login");
+                        }
+                    }//end if user undefined
                     else{
                         log('[*] Unknown user '+uid+' asked for password reset, maybe someone is trying to do something nasty.', app_log);
-                        const transporter = nodemailer.createTransport({
-                            port: 25,
-                            host: 'localhost',
-                            tls: {
-                                rejectUnauthorized: false
-                            }
-                         });
                          //send mail to admin
                          text="User "+uid+" asked for a password reset, this user cannot be found in system maybe someone is trying to do something nasty.\nRequest occurred from: "+req.ip.replace(/f/g, "").replace(/:/g, "")+" User agent: "+req.get('User-Agent');
-                         admin_mail = "SKDC <"+config.maics.admin_mail+">"
+                         admin_mail = "MAICS <"+config.maics.admin_mail+">"
                          admins_mail = "Admins <"+config.maics.admin_mail+">"
                          var message = {
                             from: admin_mail,
                             to: admins_mail,
-                            subject: "SKDC - Unexistent user "+uid+" asked for password reset",
+                            subject: "MAICS - Unexistent user "+uid+" asked for password reset",
                             html: util.format(mail.noreset , text)
                          };
                          transporter.sendMail(message, (error, info) => {
