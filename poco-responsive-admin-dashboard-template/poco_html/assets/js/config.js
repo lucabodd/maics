20439103465837598d2d20406d514ddc7bc59512
Luca Bodini <lucabodd@gmail.com>
2021-02-01 10:40:08 +0100

    removing template files

--- poco-responsive-admin-dashboard-template/poco_html/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-var primary = localStorage.getItem("primary") || '#7e37d8';
-var secondary = localStorage.getItem("secondary") || '#1ea6ec';
-
-window.pocoAdminConfig = {
-	// Theme Primary Color
-	primary: primary,
-	// theme secondary color
-	secondary: secondary,
-};
